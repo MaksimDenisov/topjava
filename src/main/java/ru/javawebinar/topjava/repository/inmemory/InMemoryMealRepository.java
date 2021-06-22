c315c16b6ef4c56b4b24e0df5ebac795b9959739
MaksimDenisov <max.nick.demax@gmail.com>
2021-06-22 11:12:14 +0300

    complete optional

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -4,43 +4,56 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> this.save(1, meal));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
+        repository.putIfAbsent(userId, new ConcurrentHashMap<>());
+        Map<Integer, Meal> map = repository.get(userId);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            map.put(meal.getId(), meal);
             return meal;
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return map.computeIfPresent(meal.getId(), (id, oldMeal)-> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int userId, int id) {
+        Map<Integer, Meal> map = repository.get(userId);
+        if (map == null)
+            return false;
+        return map.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int id) {
+        Map<Integer, Meal> map = repository.get(userId);
+        if (map == null)
+            return null;
+        return map.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll(int userId) {
+        Map<Integer, Meal> map = repository.get(userId);
+        if (map == null)
+            return Collections.emptyList();
+        return map.values().stream()
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,44 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.List;
 
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal save(int userId, Meal meal) {
+        return repository.save(userId, meal);
+    }
+
+    public boolean delete(int userId, int id) {
+        return checkNotFound(id, repository.delete(userId, id));
+    }
+
+    public Meal get(int userId, int id) {
+        return checkNotFound(id, repository.get(userId, id));
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    private boolean checkNotFound(int id, boolean check) {
+        if (!check)
+            throw new NotFoundException("Not found with id = " + id);
+        return true;
+    }
+
+    private <T> T checkNotFound(int id, T check) {
+        if (check == null)
+            throw new NotFoundException("Not found with id = " + id);
+        return check;
+    }
 }
