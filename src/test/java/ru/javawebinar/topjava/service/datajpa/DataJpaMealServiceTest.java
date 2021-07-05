76ff94ca2d3daa8f93cabb7721d52e2be3010140
MaksimDenisov <max.nick.demax@gmail.com>
2021-07-05 11:37:43 +0300

    complete optional

---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+
+@ActiveProfiles(Profiles.DATAJPA)
+public class DataJpaMealServiceTest extends AbstractMealServiceTest {
+
+    @Test
+    public void getMealWithUser(){
+        Meal actual = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
+        MealTestData.MATCHER.assertMatch(actual, adminMeal1);
+        UserTestData.MATCHER.assertMatch(actual.getUser(), UserTestData.admin);
+    }
+}
